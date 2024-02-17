a4c80c4beec345d0fcfa19cc5006f2c7c30fde96
Omprakash Rar <raromprakash4@gmail.com>
2024-02-17 19:15:32 +0530

    updated
    Pokemon name, image , type , id is updated

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState} from "react";
 import axios from "axios";
 import './PokemonList.css'
+import Pokemon from "../Pokemon/Pokemon.jsx";
 
 function PokemonList() {
 
@@ -12,7 +13,18 @@ function PokemonList() {
         const pokemonResults = response.data.results;
         const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
         const pokemonData = await axios.all(pokemonResultPromise);
-        console.log(pokemonData.data);
+        console.log(pokemonData);
+        const res = pokemonData.map((pokeData) => {
+            const pokemon = pokeData.data;
+            return {
+                id : pokemon.id,
+                name : pokemon.name,
+                image : (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
+                types : pokemon.type,
+            }
+        });
+        console.log(res);
+        setPokemonList(res);
       setIsLoading(false);
     }
 
@@ -21,13 +33,14 @@ useEffect(() => {
     downloadPokemons();
 },[])
 
-    
 
 return (
     <div className="pokemon-list-wrapper">
 
          <div>Pokemon</div> 
-        {(isLoading) ? 'Loading...' : 'Data Downloaded'}
+        {(isLoading) ? 'Loading...' : 
+        pokemonList.map((p) => <Pokemon name= {p.name} image= {p.image} key = {p.id}  />)
+    }
 
     </div>
 )
